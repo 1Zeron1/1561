016d6e71b96f476cd81d99cfba588547a33d83cb
1Zeron1 <143997767+1Zeron1@users.noreply.github.com>
2023-09-04 11:54:38 +0300

    Add files via upload

---
+++ easy.h
@@ -0,0 +1,10 @@
+#ifndef MODULE_H_INCLUDED
+#define MODULE_H_INCLUDED
+#include <iostream>
+void itc_fio(void);
+void itc_name(void);
+
+
+
+#endif // MODULE_H_INCLUDED
+
---
+++ modul.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+
+void itc_fio(void){
+    cout<<"Zhurov\n";
+    itc_name();
+    cout<< "Dmitrievich\n";
+
+
+}
+
+void itc_name(void) {
+    cout<< "Egor\n";
+
+}
